08c0ea5b7fd905809541adbb7739d8b1eb465e6a
Benny Mei <meibenny@gmail.com>
2015-03-24 09:14:09 -0300

    Constructing the URI works

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -51,6 +51,8 @@ class OracleOfBacon
   def make_uri_from_arguments
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
+    @uri = 'http://oracleofbacon.org/cgi-bin/xml?p='+CGI.escape(api_key)+
+            '&a=' + CGI.escape(from) + '&b=' + CGI.escape(to)
   end
       
   class Response
